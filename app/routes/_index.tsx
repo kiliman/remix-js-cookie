1635671fdc9f68aa0559f9a65de42325d5e2ff3f
kiliman <kiliman@gmail.com>
2024-06-20 10:01:23 -0500

    Add js-cookie for client-side cookies

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,8 @@
-import type { MetaFunction } from "@remix-run/node";
+import { LoaderFunctionArgs, MetaFunction, json } from "@remix-run/node";
+import Cookies from "js-cookie";
+import cookie from "cookie";
+import { useLoaderData } from "@remix-run/react/dist/components";
+import { useRevalidator } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -7,42 +11,34 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+export function loader({ request }: LoaderFunctionArgs) {
+  const cookies = cookie.parse(request.headers.get("cookie") ?? "");
+  return json({ cookies });
+}
 export default function Index() {
+  const data = useLoaderData<typeof loader>();
+  const { revalidate } = useRevalidator();
+  const setTheme = (theme: string) => {
+    Cookies.set("theme", theme);
+    console.log("setting theme", document.cookie);
+    revalidate();
+  };
   return (
     <div className="font-sans p-4">
       <h1 className="text-3xl">Welcome to Remix</h1>
-      <ul className="list-disc mt-4 pl-6 space-y-2">
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/quickstart"
-            rel="noreferrer"
-          >
-            5m Quick Start
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/tutorial"
-            rel="noreferrer"
-          >
-            30m Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/docs"
-            rel="noreferrer"
-          >
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+      <button
+        className="bg-gray-900 text-white px-2 py-1 rounded"
+        onClick={() => setTheme("dark")}
+      >
+        Dark Theme
+      </button>
+      <button
+        className="bg-gray-300 text-gray-900 px-2 py-1 rounded"
+        onClick={() => setTheme("light")}
+      >
+        Light Theme
+      </button>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
   );
 }
